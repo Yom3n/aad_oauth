61710b686c0b2c0b597b9096a5546b50871530e3
Yomen <kamilbudnik94@gmail.com>
2021-04-26 10:31:51 +0200

    updated deps to support Flutter 2.0

--- example_b2c/test/widget_test.dart
+++ example_b2c/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:aad_oauth_example_b2c/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
